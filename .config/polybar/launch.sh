e689ff7476ccecb73f5ce2ea8d49df44de8b6e72
Lior Leo Hakim <lior@hourone.ai>
2019-03-05 13:20:16 +0200

    dynamic network

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
-
+export DEFAULT_NETWORK_INTERFACE=$(ls /sys/class/net | grep wlp | head -n 1 | awk '{print $1}')
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
